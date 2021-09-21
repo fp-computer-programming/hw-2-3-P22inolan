36269893c8ab932d8e9631ffbb5edfd917b506dc
P22inolan <p22inolan@fairfieldprep.org>
2021-09-21 10:17:10 -0400

    HW WIP 9/21

---
+++ hw2-3-2.py
@@ -0,0 +1,9 @@
+# Author: IBN (AMDG) 9/21/2021
+radius = 4.6
+pi = 3.14
+
+area = pi * (radius ** 2)
+print(area)
+
+perimeter = 2 * pi * radius
+print(perimeter)
---
+++ hw2-3-3.py
@@ -0,0 +1,6 @@
+# Author: IBN (AMDG) 9/21/2021
+length = 8.2
+width = 13.9
+
+area = length * width
+print(area)
