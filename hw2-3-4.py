0e715c8b902da62c6622d7d5f8efab587ad7c005
P22inolan <p22inolan@fairfieldprep.org>
2021-09-23 10:10:58 -0400

    Hw Submit 9/23

---
+++ hw2-3-4.py
@@ -0,0 +1,7 @@
+# Author: IBN (AMDG) 9/23/2021
+population_23_2020 = 331832432
+
+population_24_2021 = population_23_2020 + ((31536000 + 86400)/5)
+# Population on the 23rd plus the amount of seconds in a year and a day divided by one person every 25 seconds.
+
+print(population_24_2021)
